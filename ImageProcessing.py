454331ccad0aa4b49f647c5def95d3e77ff4493b
nguyentuthanhdn <44814087+nguyentuthanhdn@users.noreply.github.com>
2020-11-08 11:39:53 -0700

    Small change

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -16,14 +16,15 @@ class ImageProcessing():
         if displayImg:
             cv2.imshow("Me diitect hoomans", frame)
             cv2.waitKey(0)
-            self.camera.release();
-            cv2.destroyAllWindows();
 
         frame = cv2.resize(frame, (self.width, self.length))
         cameraImg = []
         cameraImg.append(frame)
         cameraImg = np.array(cameraImg, np.uint8)
+        self.camera.release();
+        cv2.destroyAllWindows();
         return cameraImg
+
 
 
 
